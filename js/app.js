679a9000e16d00a49b2764afa70b8cf57fcdc0dd
Geovane Lima <geolimass03@gmail.com>
2023-11-02 23:08:47 -0300

    Update_0.0.4 - Criando as arestas

--- js/app.js
+++ js/app.js
@@ -5,42 +5,42 @@ $('.circle').each(function () {
 
     $(this).on('click', function () { 
 
-        var vert = $(this).text();
-    
+        var vertId = $(this).attr('id').split('-')[1];
+        var vert = $(this).attr('id');
         console.log("O vértice " + vert + " foi clicado");
-    });
 
+        var cyNode = cy.getElementById(vertId);
+
+        if (!sourceNode || sourceNode === vertId) {
+            if (sourceNode === vertId) {
+                cyNode.unselect();
+                sourceNode = null;
+            } else {
+                sourceNode = vertId;
+                cyNode.select();
+            }
+        } else {
+            if (!targetNode) {
+                targetNode = vertId;
+                cyNode.select();
+                
+                findShortestPath(sourceNode, targetNode);
+    
+                cy.elements().unselect();
+                sourceNode = null;
+                targetNode = null;
+            }
+        }
+    });
 });
 
-
-document.getElementById('universityMap').addEventListener('click', function(event) {
-    
-    clickCount++;
-    const x = event.offsetX;
-    const y = event.offsetY;
-
-    const clickedNodeId = getNodeByCoordinates(x, y);
-
-    console.log(`Clicked Node ID: ${clickedNodeId}`);
-
-    if (!clickedNodeId){
-        clickCount = 0
-        return;
-    } 
-
-    if (clickCount === 1) {
-        sourceNode = clickedNodeId;
-    } else if (clickCount === 2) {
-        targetNode = clickedNodeId;
-        
-        findShortestPath(sourceNode, targetNode);
-        clickCount = 0; 
-    }
+document.addEventListener('click', function(e) {
+    console.log('X:', e.clientX, 'Y:', e.clientY);
 });
 
 
 let cy = cytoscape({
-    container: document.getElementById('grafo'),
+    headless: true,
     elements: [
         { data: { id: '1' } },
         { data: { id: '2' } },
@@ -145,42 +145,6 @@ function findShortestPath(sourceId, targetId) {
     return path;
 }
 
-function getNodeByCoordinates(x, y) {
-    const nodes = {
-        "1": { x: 45, y: 30 },
-        "2": { x: 170, y: 60 },
-        "3": { x: 230, y: 110 },
-        "4": { x: 390, y: 30 },
-        "5": { x: 400, y: 245 },
-        "6": { x: 385, y: 330 },
-        "7": { x: 330, y: 375 },
-        "8": { x: 280, y: 350 },
-        "9": { x: 250, y: 285 },
-        "10": { x: 240, y: 205 },
-        "11": { x: 205, y: 155 },
-        "12": { x: 190, y: 220 },
-        "13": { x: 160, y: 260 },
-        "14": { x: 140, y: 320 },
-        "15": { x: 155, y: 295 },
-        "16": { x: 100, y: 250 },
-        "17": { x: 70, y: 195 },
-        "18": { x: 50, y: 170 },
-        "19": { x: 60, y: 115 },
-        "20": { x: 105, y: 155 },
-        "21": { x: 140, y: 185 },
-        "22": { x: 90, y: 185 },
-        "23": { x: 205, y: 270 },
-        "24": { x: 275, y: 330 }
-    };
-
-    for (let nodeId in nodes) {
-        const node = nodes[nodeId];
-        if (Math.abs(node.x - x) < 10 && Math.abs(node.y - y) < 10) {
-            return nodeId;
-        }
-    }
-    return null;
-}
 
 
 
